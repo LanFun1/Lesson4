ae8ec9638ba7b944e749ac071b22991959aa5549
LanFun1 <66485374+LanFun1@users.noreply.github.com>
2020-07-08 08:57:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = Convert.ToInt32(Console.ReadLine());
+            int doctorAppointment = 10;
+            int waitingHour = (people * doctorAppointment)/60 ;
+            int waitingMinute = (people * doctorAppointment) % 60;
+            Console.WriteLine(waitingHour+" часа "+waitingMinute+" минут");
+        }
+    }
+}
